f93d22baf1c0ec3c32337f6a5a17491e1438c0e8
Matt Miller <matt@miller-media.com>
2023-05-20 00:37:17 -0700

    Added tarball to Version 0.0.1

--- Formula/kneejerk.rb
+++ Formula/kneejerk.rb
@@ -4,7 +4,7 @@ class Kneejerk < Formula
   desc "A tool for scanning environment variables from React websites"
   homepage "https://github.com/MillerMedia/Kneejerk"
   url "https://github.com/MillerMedia/Kneejerk/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "your_tarball_sha256"
+  sha256 "cbaa6cf770b5867f4967eac62c2293dfe94e2e18e6a3f3b4131308df42bb51c5"
 
   depends_on "python@3.8"
 
