ceb03caed071b363b83f619096959412edbbdf0d
Matt Miller <matt@miller-media.com>
2023-05-20 01:13:24 -0700

    Update kneejerk.rb

--- Formula/kneejerk.rb
+++ Formula/kneejerk.rb
@@ -4,18 +4,16 @@ class Kneejerk < Formula
   desc "A tool for scanning environment variables from React websites"
   homepage "https://github.com/MillerMedia/Kneejerk"
   url "https://github.com/MillerMedia/Kneejerk/archive/refs/tags/0.0.1.tar.gz"
+  version "0.0.1"
   sha256 "507fec4b595bfedee3993349af31c13d517375bccb2c3db79b6a58af75364bde"
 
   depends_on "python@3.9"
 
   def install
-    venv = virtualenv_create(libexec, Formula["python@3.9"].opt_bin/"python3")
-    system libexec/"bin/pip", "install", "-v", "--no-deps", buildpath
-    system libexec/"bin/pip", "uninstall", "-y", "kneejerk"
-    venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
-    assert_match "0.0.1", shell_output("#{bin}/kneejerk --version")
+    system bin/"kneejerk", "-h"
   end
 end
