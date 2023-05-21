0424cf0bef1d9256b0ecf82d1546bef228fe2279
Matt Miller <matt@miller-media.com>
2023-05-20 19:00:36 -0700

    Version 0.1.5

--- Formula/kneejerk.rb
+++ Formula/kneejerk.rb
@@ -1,9 +1,9 @@
 class Kneejerk < Formula
   desc "A tool for scanning environment variables from React websites"
   homepage "https://github.com/MillerMedia/Kneejerk"
-  url "https://github.com/MillerMedia/kneejerk/archive/refs/tags/v0.1.4.tar.gz"
-  version "0.1.4"
-  sha256 "663c7f6ee8b0e3cc1ad8ca8ffe6c3e20e13322eebfe1b0850173ce661deee1ec"
+  url "https://github.com/MillerMedia/kneejerk/archive/refs/tags/v0.1.5.tar.gz"
+  version "0.1.5"
+  sha256 "6c14c47fdb1ceaa418cfa4c6698a27a22952b69ddcdfe1c587c5c1110767e07b"
 
   depends_on "go" => :build
 
