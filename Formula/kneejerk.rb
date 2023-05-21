60b7c8236906e2de9afb13beaa3c5cd17a04b679
Matt Miller <matt@miller-media.com>
2023-05-20 17:29:42 -0700

    Update code to Go language

--- Formula/kneejerk.rb
+++ Formula/kneejerk.rb
@@ -1,16 +1,14 @@
 class Kneejerk < Formula
-  include Language::Python::Virtualenv
-
   desc "A tool for scanning environment variables from React websites"
   homepage "https://github.com/MillerMedia/Kneejerk"
-  url "https://github.com/MillerMedia/Kneejerk/archive/refs/tags/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "080c2c41319f514b99ab7d757b4acad6ae58fc5ddbedb81cd42584f240c14fb9"
+  url "https://github.com/MillerMedia/Kneejerk/archive/refs/tags/0.1.tar.gz"
+  version "0.1"
+  sha256 "de5955c932ff2c07988cb37607b4ff7ba9390bfc39313618baafe8776bc9e7e2"
 
-  depends_on "python@3.9"
+  depends_on "go" => :build
 
   def install
-    virtualenv_install_with_resources
+    system "go", "build", "-o", bin/"kneejerk", "."
   end
 
   test do
